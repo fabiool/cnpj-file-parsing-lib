fb6792efb0cf22554097d237d84124323cc66c70
Fabio Oliveira <fabio.oliveira@tatic.net>
2019-03-16 12:01:23 -0300

    Pristing Spring Boot project

---
+++ src/main/java/eu/fabiool/cnpj/file/parsing/lib/CnpjFileParsingLibApplication.java
@@ -0,0 +1,13 @@
+package eu.fabiool.cnpj.file.parsing.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnpjFileParsingLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnpjFileParsingLibApplication.class, args);
+	}
+
+}
