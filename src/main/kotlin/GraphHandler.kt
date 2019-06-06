af56fe59075b97f42988edbf3ac128d431ff284a
Matheus Vargas <m6.vargas@gmail.com>
2019-06-06 12:01:00 -0300

    Estruturação para implementação do JanusGraph

--- src/main/kotlin/GraphHandler.kt
+++ src/main/kotlin/GraphHandler.kt
@@ -4,6 +4,7 @@ import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSo
 import org.apache.tinkerpop.gremlin.structure.Graph
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory
 import org.janusgraph.core.JanusGraph
+import org.janusgraph.core.Multiplicity
 import org.janusgraph.core.schema.JanusGraphManagement
 import java.util.logging.Level
 import java.util.logging.Logger
@@ -41,7 +42,42 @@ class GraphHandler {
     }
 
     fun CreateVertexLabels(mgmt : JanusGraphManagement) {
-        mgmt.makeVertexLabel("Sede em")
+        mgmt.makeVertexLabel("Matriz").make()
+        mgmt.makeVertexLabel("Filial").make()
+        mgmt.makeVertexLabel("Sócio").make()
+    }
+
+    fun CreateEdgeLabels(mgmt: JanusGraphManagement) {
+        mgmt.makeEdgeLabel("AC").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("AL").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("AM").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("AP").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("BA").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("BR").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("CE").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("DF").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("ES").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("EX").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("GO").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("MA").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("MG").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("MS").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("MT").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("PA").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("PB").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("PE").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("PI").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("PR").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("RJ").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("RN").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("RO").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("RR").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("RS").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("SC").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("SE").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("SP").multiplicity(Multiplicity.MANY2ONE).make()
+        mgmt.makeEdgeLabel("TO").multiplicity(Multiplicity.MANY2ONE).make()
+
     }
 }
 
