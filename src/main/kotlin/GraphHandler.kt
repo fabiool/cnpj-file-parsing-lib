f9c79591844a45f7fc4bb2aa386ff6d0361e5c00
Matheus Vargas <m6.vargas@gmail.com>
2019-06-06 11:15:28 -0300

    Estruturação para implementação do JanusGraph

--- src/main/kotlin/GraphHandler.kt
+++ src/main/kotlin/GraphHandler.kt
@@ -3,34 +3,45 @@ import org.apache.commons.configuration.PropertiesConfiguration
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
 import org.apache.tinkerpop.gremlin.structure.Graph
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory
+import org.janusgraph.core.JanusGraph
+import org.janusgraph.core.schema.JanusGraphManagement
 import java.util.logging.Level
 import java.util.logging.Logger
+
 
 class GraphHandler {
 
     lateinit var graph : Graph
     lateinit var g : GraphTraversalSource
-
+    val LOGGER : Logger = Logger.getLogger(javaClass.toString())
 
     fun StartGraphProperties(){
 
-        val LOGGER : Logger = Logger.getLogger(javaClass.toString())
         LOGGER.log(Level.INFO, "Starting JanusGraph properties")
 
         val conf : Configuration = PropertiesConfiguration("conf/janusgraph-inmemory-tatic.properties")
         graph = GraphFactory.open(conf)
         g = graph.traversal()
 
+        var mgmt : JanusGraphManagement = GetJanusGraph().openManagement()
+
         CloseGraph()
     }
 
 
-    fun writeDataToGraph() {
+    fun GetJanusGraph() : JanusGraph{
+        return graph as JanusGraph
     }
 
     fun CloseGraph() {
+
+        LOGGER.log(Level.INFO, "Closing graph")
         graph.close()
         g.close()
+    }
+
+    fun CreateVertexLabels(mgmt : JanusGraphManagement) {
+        mgmt.makeVertexLabel("Sede em")
     }
 }
 
